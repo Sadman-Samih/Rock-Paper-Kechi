a9a43fd644bafb2dcb29602af3c606f9cf21b1c8
Sadman <sami20112012000@gmail.com>
2026-06-22 18:28:56 +0600

    js updated

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function getComputerChoice() {
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    const Choice = prompt("Rock, Paper Or Kechi?");
+    return "Choice";
+
+}
+
+console.log(getHumanChoice());
+
+let HumanScore = 0;
+let ComputerScore = 0;
+
+function playRound(HumanChoice, ComputerChoice) {
+    const Human = HumanChoice.ToLowerCase();
+    const Computer = ComputerChoice;
+
+    console.log(`Human: ${Human}`, `Computer: ${Computer}`)
+
+    if (Human === Computer) {
+        return "Its a tie!"
+    }
+
+    else if ((Human === "Rock" && Computer === "Kechi") || (Human === "Paper" && Computer === "Rock") || (Human === "Kechi" && Computer === "Paper")) {
+    }
+}
+
