7f0e373cf97e28bdd0a009d0ad10777ea9cab3e5
Sadman <sami20112012000@gmail.com>
2026-06-26 22:55:16 +0600

    yay

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
+const humanScore = (0)
+const computerScore = (0)
+
+
+
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3)
-    if (random <= 1) {
+    if (random <= 0) {
         return "Rock";
-    } else if (random <= 2) {
+    } else if (random <= 1) {
         return "Paper";
-    } else {
+    } else if (random <= 2) {
         return "Kechi";
     }
 }
 
-i
+function getHumanChoice() {
+    prompt("Rock, Paper, or Kechi");
+}
+
+function playRound(humanChoice, computerChoice) {
+    const correction = humanChoice.toLowerCase()
+
+    //Human Win Logic
+    if (humanChoice === computerChoice) {
+        return "Draw!";
+    } else if (humanChoice === "Rock" && computerChoice === "Kechi") {
+        return "You Win !";
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        return "You Win !";
+    } else if (humanChoice === "Kechi" && computerChoice === "Paper") {
+        return "You Win !";
+    }
+
+
+    //Computer Win Logic
+    if (humanChoice === computerChoice) {
+        return "Draw !";
+    } else if (computerChoice === "Rock" && humanChoice === "Kechi") {
+        return "Computer Wins ! ";
+    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+        return "Computer Wins !";
+    } else if (computerChoice === "Kechi" && humanChoice === "Paper") {
+        return "Computer Wins !";
+    }
+
+}
+
